3869c906eca4b2ee65b3829d2b6385b42dc9db15
stephenhky <stephenhky@yahoo.com.hk>
2016-06-17 12:10:42 -0400

    Price quench annotator

---
+++ deepmodels/PriceQuenchAnnotation.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def annotate_sharpdrop(prices,                 # ndarray
+                       future_window=5,        # number of days to include
+                       drop_threshold=0.01,    # drop threshold
+                       drop_window=2,          # drop window
+                       percentage=True         # use percentage if True; otherwise, absolute number
+                      ):
+    "Return 1 if there will be a sharp drop; 0 if not; -1 if invalid."
+    flags = np.repeat(-1, len(prices))
+    for idx in range(0, len(prices)-future_window):
+        flag = 0
+        for kdx in range(idx, idx+future_window-drop_window):
+            absdrop = prices[idx]-prices[idx+drop_window-1]
+            if absdrop / (prices[idx] if percentage else 1) > drop_threshold:
+                flag = 1
+            if flag > 0: break
+        flags[idx] = flag
+    return flags
